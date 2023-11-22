ccb97b5e177d3945ded569c57de506791abb9741
Frank Acquah <acquahfrank25@gmail.com>
2023-11-22 09:37:55 +0000

    Did work on how to handle UI data in Jmix

--- src/main/java/com/company/onboarding/view/user/UserDetailView.java
+++ src/main/java/com/company/onboarding/view/user/UserDetailView.java
@@ -1,13 +1,21 @@
 package com.company.onboarding.view.user;
 
 import com.company.onboarding.entity.OnboardingStatus;
+import com.company.onboarding.entity.Step;
 import com.company.onboarding.entity.User;
+import com.company.onboarding.entity.UserStep;
 import com.company.onboarding.view.main.MainView;
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
 import io.jmix.core.EntityStates;
+import io.jmix.flowui.Notifications;
 import io.jmix.flowui.component.textfield.TypedTextField;
+import io.jmix.flowui.model.CollectionPropertyContainer;
+import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.view.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,6 +45,18 @@ public class UserDetailView extends StandardDetailView<User> {
     private MessageBundle messageBundle;
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private DataManager dataManager;
+
+    @Autowired
+    private Notifications notifications;
+
+    @ViewComponent
+    private DataContext dataContext;
+
+    @ViewComponent
+    private CollectionPropertyContainer<UserStep> stepsDc;
 
     @Subscribe
     public void onInit(final InitEvent event) {
@@ -71,6 +91,28 @@ public class UserDetailView extends StandardDetailView<User> {
     protected void onBeforeSave(final BeforeSaveEvent event) {
         if (entityStates.isNew(getEditedEntity())) {
             getEditedEntity().setPassword(passwordEncoder.encode(passwordField.getValue()));
+        }
+    }
+
+    @Subscribe("generateButton")
+    public  void onGenerateButtonClick(final ClickEvent<Button> event){
+        User user = getEditedEntity();
+        if(user.getJoiningDate() == null){
+            notifications.create("Cannot generate steps for user without joining date").show();
+            return;
+        }
+        List<Step> steps = dataManager.load(Step.class)
+                .query("select s from Step s order by s.sortValue asc")
+                .list();
+        for(Step step : steps){
+            if(stepsDc.getItems().stream().noneMatch(userStep -> userStep.getStep().equals(step))){
+                UserStep userStep = dataContext.create(UserStep.class);
+                userStep.setUser(user);
+                userStep.setStep(step);
+                userStep.setDueDate(user.getJoiningDate().plusDays(step.getDuration()));
+                userStep.setSortValue(step.getSortValue());
+                stepsDc.getMutableItems().add(userStep);
+            }
         }
     }
 }
